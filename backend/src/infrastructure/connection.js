67a777f26e458ca6390beb761f04bb359f343636
Thiago Alberto da Silva <alrrtas@gmail.com>
2021-10-23 16:43:24 -0300

    Creating default values for sellers

--- backend/src/infrastructure/connection.js
+++ backend/src/infrastructure/connection.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-    host : 'localhost',
-    // host : '172.17.0.1',
+    //host : 'localhost',
+    host : '172.17.0.1',
     port : 3306,
     user : 'root',
     password : 'RootPassword',
