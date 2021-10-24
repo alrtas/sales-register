14e2bfe806e781b911260ef4bb107fa614977314
Thiago Alberto da Silva <alrrtas@gmail.com>
2021-10-23 23:34:57 -0300

    Creating basic crd app for sales and get best sellers

--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component }     from 'react';
+import { Container, Row, Col }  from 'reactstrap';
+import ModalForm        from './Components/Modals/Modal';
+import DataTable        from './Components/Tables/DataTable';
+import DataTableSellers from './Components/Tables/DataTableSellers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    items: [],
+    sellers: []
+  }
+
+  getSellers(){
+    fetch('http://172.17.0.2:3000/sellers/values-list/')
+      .then(response => response.json())
+      .then(items => {
+        this.setState(({
+          sellers: items
+        }))
+      })
+      .catch(err => console.log(err))
+  }
+
+  getItems(){
+    fetch('http://172.17.0.2:3000/sales/')
+      .then(response => response.json())
+      .then(items => {
+        this.setState({items})
+      })
+      .catch(err => console.log(err))
+  }
+
+  addItemToState = (item) => {
+    this.setState(prevState => ({
+      items: [...prevState.items, item]
+    }))
+  }
+
+  updateState = (item) => {
+    const itemIndex = this.state.items.findIndex(data => data.id === item.id)
+    const newArray = [
+    // destructure all items from beginning to the indexed item
+      ...this.state.items.slice(0, itemIndex),
+    // add the updated item to the array
+      item,
+    // add the rest of the items to the array from the index after the replaced item
+      ...this.state.items.slice(itemIndex + 1)
+    ]
+    this.setState({ items: newArray })
+  }
+
+  deleteItemFromState = (id) => {
+    const updatedItems = this.state.items.filter(item => item.id !== id)
+    this.setState({ items: updatedItems })
+  }
+
+  componentDidMount(){
+    this.getItems()
+    this.getSellers()
+  }
+
+  render() {
+    return (
+      <Container className="App">
+        <Row>
+          <Col>
+            <h1 style={{margin: "20px 0"}}>Sales</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+          
+            <DataTable items={this.state.items} updateState={this.updateState} deleteItemFromState={this.deleteItemFromState} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ModalForm buttonLabel="Add Sale" addItemToState={this.addItemToState}/>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <br/> <br/>
+            <h1 style={{margin: "20px 0"}}>Top Sallers</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <DataTableSellers items={this.state.sellers} />
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ frontend/web/src/Components/Tables/DataTable.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import { Table, Button } from 'reactstrap';
+import ModalForm from '../Modals/Modal'
+
+class DataTable extends Component {
+  deleteItem = idSales => {
+    let confirmDelete = window.confirm('Delete sale forever?')
+    if(confirmDelete){
+      console.log(idSales)
+      fetch(`http://172.17.0.2:3000/sales/${idSales}`, {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(item => {
+        this.props.deleteItemFromState(idSales)
+        window.location.reload();
+      })
+      .catch(err => console.log(err))
+    }
+
+  }
+
+  render() {
+    const items = this.props.items.map(item => {
+      
+      return (
+        <tr key={item.idSales}>
+          <th scope="row">{item.idSales}</th>
+          <td>{item.sellerName}</td>
+          <td>{item.customerName}</td>
+          <td>{item.dateOfSale}</td>
+          <td>{item.saleItemName}</td>
+          <td>{item.saleValue}</td>
+          <td>
+            <div style={{width:"110px"}}>
+              {/* <ModalForm buttonLabel="Edit" item={item} updateState={this.props.updateState}/> */}
+              {' '}
+              <Button color="danger" onClick={() => this.deleteItem(item.idSales)}>Del</Button>
+            </div>
+          </td>
+        </tr>
+        )
+      })
+
+    return (
+      <Table responsive hover>
+        <thead>
+          <tr>
+            <th>Sale ID</th>
+            <th>Seller Name</th>
+            <th>Customer Name</th>
+            <th>Date of Sale</th>
+            <th>Item</th>   
+            <th>Item price</th>   
+          </tr>
+        </thead>
+        <tbody>
+          {items}
+        </tbody>
+      </Table>
+    )
+  }
+}
+
+export default DataTable
