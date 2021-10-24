1ca3b7c84e8d97c1ad77a9f020fc409e85237f80
Thiago Alberto da Silva <alrrtas@gmail.com>
2021-10-23 23:34:14 -0300

    Update cors policy

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,12 +17,22 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors())
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, OPTIONS, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+  res.setHeader('Access-Control-Allow-Credentials', false);
+  // Pass to next layer of middleware
+  next();
+});
+
 
 connection.connect((erro)=>{
   if(erro){
